3eb23e1390a40869b807f10dc29f3226ae8b33a3
GitHub Action <action@github.com>
2025-04-13 11:07:46 +0000

    Initialize template repository proper
    Set package name to com.github.blackcat-code-dev.zoom
    Set project name to Zoom
    Set modid to zoom

--- src/main/java/com/github/blackcat-code-dev/zoom/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/blackcat-code-dev/zoom/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.blackcat-code-dev.zoom.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/blackcat-code-dev/zoom/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/blackcat-code-dev/zoom/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.blackcat-code-dev.zoom.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
